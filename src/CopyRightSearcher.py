01f24d28281ae3198bee26584f11deda2dcc3101
ytyaru0 <ytyaru0@outlook.jp>
2018-04-11 14:59:00 +0900

    URLとCopyrightを返すよう修正

--- src/CopyRightSearcher.py
+++ src/CopyRightSearcher.py
@@ -23,59 +23,42 @@ class CopyRightSearcher:
         self.__LoadCache()
 
     def Search(self, metadata:DistributionMetadata):
-        license_url = self.__FindLicenseUrlFromCache(metadata)
+        license_url, copyright = self.__FindLicenseUrlFromCache(metadata)
         #license_url = self.__FindLicenseUrlFromCache(metadata.url)
         if license_url is None: return self.__Search(metadata)
-        elif '' == license_url: return None # cacheにあるが昔探して見つけられなかった
-        elif license_url is not None: return license_url # cacheにある
+        elif '' == license_url: return (None, None) # cacheにあるが昔探して見つけられなかった
+        elif license_url is not None: return (license_url, copyright)  # cacheにある
 
     def __Search(self, metadata:DistributionMetadata):
         base_url = self.__GetSearchUrl(metadata)
         for fn in self.__GetLicenseFileName():
             try:
-                #url = urllib.parse.urljoin(base_url, fn)
                 url = base_url.rstrip('/') + '/' + fn.lstrip('/')
-                #print('?', base_url, fn)
                 print(url)
                 time.sleep(1)
                 with urllib.request.urlopen(url) as res:
-                    print('LICENSEが存在した！')
                     copyright = self.__FindCopyrightString(res.read().decode('utf-8'))
-                    print(copyright)
                     self.__AppendCache(metadata, url, copyright if copyright is not None else '')
-                    return copyright
-                    #if copyright is not None: return copyright
+                    return (url, copyright)
             except urllib.error.HTTPError as e: continue
             except:
                 import traceback
                 traceback.print_exc()
         self.__AppendCache(metadata, '', '')
-        return None
+        return (None, None)
 
     # LICENSEファイルがありそうなURL
-    # サイトによって違う？: https://bitbucket.org/zzzeek/alembic/raw/a7164ef76729aa9306f9c7e2636448a588f35ddb/LICENSE
+    # サイトによって違う？: 
+    #   bitbucket: https://bitbucket.org/zzzeek/alembic/raw/a7164ef76729aa9306f9c7e2636448a588f35ddb/LICENSE
+    #   リビジョンhash値のせいでURLを特定できない
     def __GetSearchUrl(self, metadata:DistributionMetadata):
         url = urllib.parse.urlparse(metadata.url)
-        #if 'bitbucket.org' == url.netloc:
-        #if 'github.com' == url.netloc:
-        #else:
-        """
-        for u in [metadata.name, '']:
-            try:
-                url = urllib.parse.urljoin(metadata.url, u)
-                with urllib.request.urlopen(url) as res:
-                    print('*', url)
-                    return url
-            except: pass
-        """
+        #'https://raw.githubusercontent.com/pallets/markupsafe/master/LICENSE'
         if 'github.com' == url.netloc:
             url_path = url.path.rstrip('/') + '/' + 'master'
             new_url = urllib.parse.urlunparse((url.scheme,'raw.githubusercontent.com',url_path,'','',''))
             print(new_url)
             return new_url
-            #print(new_url.geturl())
-            #return new_url.geturl()
-            #'https://raw.githubusercontent.com/pallets/markupsafe/master/LICENSE'
         else:
             for u in [metadata.name, '']:
                 try:
@@ -84,7 +67,6 @@ class CopyRightSearcher:
                         print('*', url)
                         return url
                 except: pass
-   
 
     def __GetLicenseFileName(self):
         for fn in ['LICENSE', 'COPYING', 'COPYRIGHT', 'LICENCE']:
@@ -102,9 +84,10 @@ class CopyRightSearcher:
         with open(self.__path_cache) as f:
             self.__tsv = list(csv.reader(f, delimiter='\t'))
 
+    # LicenseURLとCopyrightを返す
     def __FindLicenseUrlFromCache(self, metadata):
         for r in self.__tsv:
-            if metadata.name == r[0]: return r[1]
+            if metadata.name == r[0]: return r[1], r[2]
         return None
 
     def __AppendCache(self, metadata, license_url, copyright):
@@ -115,22 +98,14 @@ class CopyRightSearcher:
 
     def __FindCopyrightString(self, text):
         print(text)
-        #'*Copyright (C)*'
-        #'*Copyright © *'
-        #'Copyright [yyyy] [name of copyright owner]'
-        #r'.*Copyright[ ]*(© |\([C|c]\))?[ ]*[0-9]{4:}.*'
-        #pattern1 = re.compile(r'.*Copyright[ ]*(© |\([C|c]\))?[ ]*[0-9]{4:}.*')
-        #pattern2 = re.compile(r'.*(© |\([C|c]\)) [ ]*[0-9]{4:}.*')
         m = self.__pattern1.match(text)
         if m is not None:
             print(m)
             return m[0]
-
         m = self.__pattern2.match(text)
         if m is not None:
             print(m)
             return m[0]
-
         return None
         # Copyright YYYY
         # Copyright ©  YYYY
